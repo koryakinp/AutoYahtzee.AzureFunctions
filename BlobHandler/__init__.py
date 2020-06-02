0002366b61aea99af76311843b7f197b196acf0f
koryakinp <koryakinp@koryakinp.com>
2020-06-02 01:14:22 -0400

    -minor fixes

--- BlobHandler/__init__.py
+++ BlobHandler/__init__.py
@@ -1,9 +1,11 @@
 import logging
 import azure.functions as func
+from pathlib import Path    
 
 
 def main(myblob: func.InputStream, msg: func.Out[str]):
-    logging.info(f"Processing Blob: {myblob.name}")
-    msg.set(myblob.name)
+    filename = Path(myblob.name).name
+    logging.info(f"Processing Blob: {filename}")
+    msg.set(filename)
 
     
